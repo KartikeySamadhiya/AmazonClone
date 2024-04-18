f4f2f30cfdb2afccbafe8a362d833540be9613ac
KartikeySamadhiya <kartikeys.ug23.cs@nitp.ac.in>
2024-04-18 11:44:02 +0530

    Slideshow added

---
+++ home.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+
+  if (slideIndex >= slides.length) { 
+    slideIndex = 0;
+  }    
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex].style.display = "block";  
+}
